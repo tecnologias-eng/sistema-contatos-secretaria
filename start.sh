3d88c0da84f873ba42ee1e42a5ff5a44bfae8137
Seu Nome <seu@email.com>
2026-02-10 20:04:25 -0300

    FIX: usa porta dinâmica do Railway

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 PORT=${PORT:-8080}
-echo "Starting Django on port $PORT"
+echo "🚀 Starting Django on port: $PORT"
 exec gunicorn sistema_contatos.wsgi:application --bind 0.0.0.0:$PORT
