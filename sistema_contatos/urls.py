c8f98d000a781a647e6b7735fcede7994395a3dd
Seu Nome <seu@email.com>
2026-02-10 12:07:18 -0300

    fix: atualiza requirements e configura urls básico

--- sistema_contatos/urls.py
+++ sistema_contatos/urls.py
@@ -1,7 +1,12 @@
+# sistema_contatos/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🚀 Django 6.0 rodando no Render!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contatos.urls')),
+    path('', home, name='home'),
 ]
